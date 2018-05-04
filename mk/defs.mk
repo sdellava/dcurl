50f9055fee1656dcafd54a13a913e5baba28853c
Alexander Sporn <github@alexsporn.de>
2018-05-05 01:31:11 +0200

    Added BUILD_FPGA option to Makefile

--- mk/defs.mk
+++ mk/defs.mk
@@ -7,6 +7,9 @@ BUILD_AVX ?= 0
 # Build OpenCL backend or not
 BUILD_GPU ?= 0
 
+# Build FPGA backend or not
+BUILD_FPGA ?= 0
+
 # Build JNI glue as the bridge between dcurl and IRI
 BUILD_JNI ?= 0
 
